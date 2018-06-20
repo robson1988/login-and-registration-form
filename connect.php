609f2af5a5a68f0707378c41b2ad77a435aa695a
robson1988 <robert.bajoo@gmail.com>
2018-06-20 18:46:42 +0100

    prepare all files to coding, basic login form done.

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+
+
+  $db_host = "localhost";
+  $db_user = "root";
+  $db_pass = "root";
+  $db_name = "github_login_and_registration";
+
+  $connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name );
