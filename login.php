66e027a2fea84117d52229d9dd1d1564894fc08a
robson1988 <robert.bajoo@gmail.com>
2018-06-22 19:57:09 +0100

    - sign in check, - error displaying when incorrect details are provided.

---
+++ login.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+  if(isset($_POST['us_submit'])) {
+
+    include_once 'connect.php';
+
+    $us_name = mysqli_real_escape_string($connect, $_POST['us_name']);
+    $us_pass = mysqli_real_escape_string($connect, $_POST['us_pass']);
+    //error handlers
+    //empty input
+    if (empty($us_name) || empty($us_pass)) {
+      $_SESSION['signup_error'] = "Ooops! U forget to fill out all fields.";
+      header("Location: index.php");
+      exit();
+    } else {
+      $sql = "SELECT * FROM users WHERE u_name='$us_name' OR u_email='$us_name'";
+      $result = mysqli_query($connect, $sql);
+      $resultCheck = mysqli_num_rows($result);
+      if ($resultCheck < 1) {
+        $_SESSION['signup_error'] = "Ooops! Username or Password doesn't match.";
+        header("Location: index.php");
+        exit();
+      } else {
+        if ($row = mysqli_fetch_assoc($result)) {
+          //CHECKING PASSWORD MATCHING
+          $dbpass = $row['u_pass'];
+          if($dbpass != $_POST['us_pass']){
+           $_SESSION['signup_error'] = "Ooops! Username or Password doesn't match.";
+           header("Location: index.php");
+           exit();
+          } elseif ($dbpass == $_POST['us_pass']) {
+            //LOGIN USER
+            $_SESSION['us_id'] = $row['u_id'];
+            $_SESSION['us_name'] = $row['u_name'];
+            $_SESSION['us_surname'] = $row['u_surname'];
+            $_SESSION['us_status'] = $row['u_status'];
+            $_SESSION['us_email'] = $row['u_email'];
+            $_SESSION['us_adress'] = $row['u_adress'];
+            $_SESSION['us_birthday'] = $row['u_birthday'];
+
+            header("Location: panel.php");
+            exit();
+          }
+        }
+      }
+    }
+  } else {
+  header("Location: index.php");
+  exit();
+}
