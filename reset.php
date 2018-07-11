f60e1dbb795e82c1eb38e3a6eaef295e4d9e7d1a
robson1988 <robert.bajoo@gmail.com>
2018-07-11 18:55:30 +0100

    user password reset done

--- reset.php
+++ reset.php
@@ -1,1 +1,34 @@
 <?php
+//ykg
+include_once 'connect.php';
+session_start();
+
+if(!isset($_POST['us_submit'])) {
+  header('Location: index.html');
+  exit();
+
+} else {
+
+  if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+      if($_POST['newPass'] == $_POST['newPassConfirm']) {
+
+        $newPass = password_hash($_POST['newPass'], PASSWORD_DEFAULT);
+
+        $username = mysqli_real_escape_string($connect, $_POST['us_username']);
+        $hash = mysqli_real_escape_string($connect, $_POST['us_hash']);
+
+        $sql = "UPDATE users SET u_pass='$newPass' WHERE u_username='$username' AND u_hash='$hash'";
+        mysqli_query($connect, $sql);
+
+        $_SESSION['msg_success'] = "Your password has been updated.";
+        header('Location: index.html');
+        exit();
+
+      } else {
+        $_SESSION['msg_error'] = "Sorry, passwords does't match. Please try again.";
+        header('Location: index.html');
+        exit();
+      }
+  }
+}
--- resetPass.php
+++ resetPass.php
@@ -3,42 +3,41 @@
 ?>
 <title> Reset Password </title>
 </head>
+
 <?php
 include_once 'connect.php';
 //check for valid data from URL
-if(isset($_GET['username']) && !empty($_GET['username']) AND isset($_GET['hash']) && !empty($_GET['hash']) AND isset($_GET['token']) && !empty($_GET['token'])) {
+if(isset($_GET['username']) && !empty($_GET['username']) && isset($_GET['hash']) && !empty($_GET['hash']) && isset($_GET['token']) && !empty($_GET['token'])) {
 
   $username = mysqli_real_escape_string($connect, $_GET['username']);
   $hash = mysqli_real_escape_string($connect, $_GET['hash']);
   $token = mysqli_real_escape_string($connect, $_GET['token']);
 
-        //check if token is still valid
-        $sql = "SELECT * FROM passres WHERE u_token='$token'";
-        $result = mysqli_query($connect, $sql);
-        $dbTimestamp = $result->fetch_assoc();
-
-        $resetTime = $dbTimestamp['u_resetTime'];
-        $date = date_create();
-        $currenTime = date_timestamp_get($date);
-        if ($currenTime - $resetTime > 3600) {
-        echo $_SESSION['msg_error'] = "Sorry, reset link already expired. Please try again.";
-        header('Location: index.html');
-        exit();
-        } else {
-  //check for existing user data and compare it with token given
-  $sql = "SELECT users.u_username = '$username', users.u_hash = '$hash' FROM users, passres WHERE passres.u_token ='$token' AND users.u_id= passres.u_id";
+  //check if token is still valid
+  $sql = "SELECT * FROM passres WHERE u_token='$token'";
   $result = mysqli_query($connect, $sql);
-  $resultCheck = mysqli_num_rows($result);
-  if($resultCheck == 0) {
-    $_SESSION['msg_error'] = "Sorry, verification failed. Please try again later.";
+  $dbTimestamp = $result->fetch_assoc();
+  $resetTime = $dbTimestamp['u_resetTime'];
+  $date = date_create();
+  $currentTime = date_timestamp_get($date);
+
+  if ($currentTime - $resetTime > 3600) {
     header('Location: index.html');
     exit();
+      } else {
+        //check for existing user data and compare it with token given
+        $sql = "SELECT users.u_username = '$username', users.u_hash = '$hash' FROM users, passres WHERE passres.u_token ='$token' AND users.u_id= passres.u_id";
+        $result = mysqli_query($connect, $sql);
+        $resultCheck = mysqli_num_rows($result);
+        if($resultCheck == 0) {
+          $_SESSION['msg_error'] = "Sorry, verification failed. Please try again later.";
+          header('Location: index.html');
+          exit();
+        }
       }
-    }
-  } else {
-      $_SESSION['msg_error'] = "Sorry, verification failed. Please try again later.";
-      header('Location: index.html');
-      exit();
+} else {
+  header('Location: index.html');
+  exit();
 }
 
 ?>
@@ -48,11 +47,12 @@ if(isset($_GET['username']) && !empty($_GET['username']) AND isset($_GET['hash']
       <div class="sign_in_form">
         <form action="reset.php" method="POST">
           <h2 class="header">Reset your Password</h2>
-          <input class="sign" type="password" name="newPass" placeholder="New Password" required>
-          <input class="sign" type="password" name="newPassCon" placeholder="Confirm New Password" required>
-          <input type="hidden" name="us_email" value="<?php $email ?>">
-          <input type="hidden" name="us_hash" value="<?php $hash ?>">
+          <input class="sign" type="password" name="newPass" placeholder="New Password">
+          <input class="sign" type="password" name="newPassConfirm" placeholder="Confirm New Password">
+          <input type="hidden" name="us_username" value="<?php echo $username; ?>">
+          <input type="hidden" name="us_hash" value="<?php echo $hash; ?>">
           <button type="submit" name="us_submit" class="btn btn-primary btn-block">Apply</button>
         </form>
       </div>
 </body>
+</html>
