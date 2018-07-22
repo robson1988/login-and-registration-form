fff5eb0902176c6f1df59e2268202f796f2fb7fb
robson1988 <robert.bajoo@gmail.com>
2018-07-22 12:02:13 +0100

    #Completed

--- includes/header.php
+++ includes/header.php
@@ -11,4 +11,3 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style.css" >
     <script src='https://www.google.com/recaptcha/api.js'></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
